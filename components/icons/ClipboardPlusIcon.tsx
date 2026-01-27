916d45738d0d725de375bd1f111b05b0baef1f82
DJRonG <Ron.maynard.jr@gmail.com>
2026-01-27 14:13:54 -0500

    Delete components directory

--- components/icons/ClipboardPlusIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const ClipboardPlusIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 0v-3.5m0 3.5a1.5 1.5 0 10-3 0m3 0a1.5 1.5 0 10-3 0M12 9.75v6.75m3.75-3.75H8.25" />
-    </svg>
-);
